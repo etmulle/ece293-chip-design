6ad6c8d9b2ae4a5f33f0ed5d6b63fec01208a1e5
etmulle <emulle@ucsc.edu>
2023-10-30 04:11:48 -0700

    Modify constants

--- src/hh.v
+++ src/hh.v
@@ -13,24 +13,24 @@ module hh #(parameter EXP = 8'b0010_1011) (
     // Check if activation changes per time step
     // Make constants parameters in model declaration
 
-    assign m_alph = ((25 - Vm) / (EXP**((25 - Vm) >> 3) - 1)) >> 3;
-    assign m_beta = 4 * EXP**(-Vm >> 4);
+    assign m_alph = ((25 - state) / (EXP**((25 - state) >> 3) - 1)) >> 3;
+    assign m_beta = 4 * EXP**(-state >> 4);
     assign m_act = m_alph / (m_alph + m_beta);
 
-    assign h_alph = EXP**(-Vm >> 4) >> 3;
-    assign h_beta = 1 / (EXP**((30 - Vm) >> 3) + 1);
+    assign h_alph = EXP**(-state >> 4) >> 3;
+    assign h_beta = 1 / (EXP**((30 - state) >> 3) + 1);
     assign h_act = h_alph / (h_alph + h_beta);
 
-    assign n_alph = ((10 - Vm) / (EXP**((10 - Vm) >> 3) - 1)) >> 7;
-    assign n_beta = EXP*(-Vm >> 6) >> 3;
+    assign n_alph = ((10 - state) / (EXP**((10 - state) >> 3) - 1)) >> 7;
+    assign n_beta = EXP*(-state >> 6) >> 3;
     assign n_act = n_alph / (n_alph + n_beta);
 
-    assign INa = m**3 * gNA * h * (Vm - ENa);
-    assign IK = n**4 * gK * (Vm - EK);
-    assign IKleak = gKleak * (Vm - EKleak);
+    assign INa = ((state - ENa)) >> 3;
+    assign IK = ((state - EK)) >> 4;
+    assign IKleak = (state - EKleak) >> 2;
 
     assign current = stim_current - INa - IK - IKleak;
-    assign next_state = state + deltaT*(current);
+    assign next_state = state + (current)>>2;
     assign spike = (state >= threshold);
 
     always @(posedge clk) begin
