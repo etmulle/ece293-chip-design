bcf3a5b742479280e083932f814315b9e03660cf
etmulle <emulle@ucsc.edu>
2023-11-02 00:56:59 -0700

    Test n update with binary num

--- src/hh.v
+++ src/hh.v
@@ -52,7 +52,7 @@ module hh #(parameter EXP = 8'b0010_1011) (
         end
         else begin
             state <= next_state;
-            n <= 2;
+            n <= 8'b0000_0001;
             // m <= next_m;
             // h <= next_h;
         end
