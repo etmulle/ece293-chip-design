6c390588d077999e6b8ced32d99293ce00531845
etmulle <emulle@ucsc.edu>
2023-11-02 06:48:14 -0700

    Attempt to fix n,m, and h updating

--- src/hh.v
+++ src/hh.v
@@ -8,7 +8,8 @@ module hh #(parameter EXP = 8'b0010_1011) (
     output wire [7:0] spike );
 
     //reg [7:0] next_state, threshold, current, INa, IK, IKleak, m_alph, m_beta, m_act, h_alph, h_beta, h_act, n_alph, n_beta, n_act;
-    reg [7:0] next_state, threshold, current, VK, VNa, Vl, n, m, h, next_n, next_m, next_h;
+    reg [7:0] next_state, threshold, current, VK, VNa, Vl, n, m, h;//, next_n, next_m, next_h;
+    wire [7:0] next_n, next_m, next_h
     // V = I/c
     // Check if activation changes per time step
     // Make constants parameters in model declaration
@@ -51,9 +52,9 @@ module hh #(parameter EXP = 8'b0010_1011) (
         end
         else begin
             state <= next_state;
-            // n <= next_n;
-            // m <= next_m;
-            // h <= next_h;
+            n <= next_n;
+            m <= next_m;
+            h <= next_h;
         end
     end
 
