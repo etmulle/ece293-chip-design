0f025cb6b35d172e96de4365f135343c8855954d
etmulle <emulle@ucsc.edu>
2023-11-02 00:48:37 -0700

    Change next_n to be wire

--- src/hh.v
+++ src/hh.v
@@ -8,7 +8,8 @@ module hh #(parameter EXP = 8'b0010_1011) (
     output wire [7:0] spike );
 
     //reg [7:0] next_state, threshold, current, INa, IK, IKleak, m_alph, m_beta, m_act, h_alph, h_beta, h_act, n_alph, n_beta, n_act;
-    reg [7:0] next_state, threshold, current, VK, VNa, Vl, n, m, h, next_n, next_m, next_h;
+    reg [7:0] next_state, threshold, current, VK, VNa, Vl, m, h, n, next_m, next_h;
+    wire [7:0] next_n;
     // V = I/c
     // Check if activation changes per time step
     // Make constants parameters in model declaration
