6b02864502e55f69430009103c09486f8470cff3
etmulle <emulle@ucsc.edu>
2023-11-02 20:57:12 -0700

    Fix spike error

--- src/hh.v
+++ src/hh.v
@@ -34,7 +34,10 @@ module hh  ( //#(parameter EXP = 8'b0010_1011)
     assign current = stim_current - (((m**3)*h*(state - -50)) >> 3) - (((n**4)*(state - 77)) >> 4) - ((state - 54) >> 2);
     // state(t+1) = state(t) + current*dt
     assign next_state = (spike[0] ? 0 : (state)) + (current >> 2);
-    assign spike[0] = (state >= threshold);
+    assign spike = {7'b0, state>= threshold};
+
+    // assign spike[0] = (state >= threshold);
+    // assign spike[7:1] = 0;
 
     // (Vm*(1-n)*(a_n) - (V_m)*n*beta_n)*dt
     assign next_n = n + (((state*(1-n)) >> 2 - (state*n) >> 2) >> 2); // replace states w/ alpha(state)
