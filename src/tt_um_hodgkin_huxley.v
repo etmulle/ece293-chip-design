44b9fba2073de9dc9d4be438e01ec177c2fa5609
etmulle <emulle@ucsc.edu>
2023-11-02 21:31:18 -0700

    Remove initialization of ena

--- src/tt_um_hodgkin_huxley.v
+++ src/tt_um_hodgkin_huxley.v
@@ -13,7 +13,7 @@ module tt_um_hodgkin_huxley  (
 
     // use bidirectionals as outputs
     assign uio_oe = 8'b11111111;
-    assign ena = 1'b1;
+    // assign ena = 1'b1;
     // assign uio_out = 8'b00000000;
 
 
